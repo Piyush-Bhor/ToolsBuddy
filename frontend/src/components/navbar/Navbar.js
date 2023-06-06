c6942d06d3ee0f18b1162dfbcb67123818845aa1
ChrisPag <chrisp99@my.yorku.ca>
2023-06-06 15:23:25 -0400

    Basic home page styles
    Added some basic home page styles and some assets

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,10 +1,14 @@
 import './navbar.css';
+import Logo from '../../assets/logo-placeholder.png'
 import { NavLink } from 'react-router-dom'
 function Navbar() {
   return (
     <div className="navbar">
         
         <ul className="pages">
+            <li>
+                <img id="logo" alt="logo" src={Logo}></img>
+            </li>
             <li>
                 <NavLink to="/">Home</NavLink>
             </li>
@@ -17,7 +21,7 @@ function Navbar() {
                 <NavLink to="/login">Login</NavLink>
             </li>
             <li>
-                <NavLink to="/signup">Sign Up</NavLink>
+                <NavLink to="/signup"><button>Sign Up</button></NavLink>
             </li>
         </ul>
         
--- frontend/src/containers/home/Home.js
+++ frontend/src/containers/home/Home.js
@@ -1,9 +1,23 @@
 import './home.css';
+import Hero from '../../assets/working.png'
 
 function Home() {
   return (
     <div className="home">
-      home
+      <div className="searchbar">
+        <p className="hero-text">Borrow and lend tools.</p>
+        <form>
+          <input type="text" placeholder="Search..." />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      <div className="hero">
+        <img alt="woodworking" src={Hero}></img>
+      </div>
+      
+      <div className="listings">
+
+      </div>
     </div>
   );
 }
