fb29830db81263669941b4dd4b19acbe6151099f
ChrisPag <chrisp99@my.yorku.ca>
2023-06-06 23:40:10 -0400

    Changed homepage design
    Changed homepage design, changed color scheme, added react-icons

--- frontend/src/containers/home/Home.js
+++ frontend/src/containers/home/Home.js
@@ -1,23 +1,78 @@
 import './home.css';
-import Hero from '../../assets/working.png'
+import Hero from '../../assets/working.png';
+import Hammer from '../../assets/hammer.jpg';
+import Wrench from '../../assets/wrench.jpg';
+import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
 
 function Home() {
+  /*test backend connection
+  const [message, setMessage] = useState("");
+
+  //when a component mounts (ie when it is inserted into the dom), call the api
+  useEffect(() => {
+    fetch("http://localhost:8080/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data));
+
+  //empty array as second argument so this hook is only called once
+  }, []);*/
   return (
     <div className="home">
-      <div className="searchbar">
+      {/*<h1>{message}</h1>*/}
+      <section className="searchbar">
         <p className="hero-text">Borrow and lend tools.</p>
         <form>
           <input type="text" placeholder="Search..." />
-          <button type="submit">Search</button>
+          <button type="submit"><FaSearch /></button>
         </form>
-      </div>
+      </section>
       <div className="hero">
         <img alt="woodworking" src={Hero}></img>
       </div>
       
-      <div className="listings">
+      <section className="listings">
+        <h2>New Listings</h2>
+        <div className="grid-container">
+          
+          <article class="single-listing">
+            <img alt="guy holding wrench" src={Wrench}></img>
+            <div className="details">
+              <p className="listing-name">Wrench</p>
+              <p className="listing-price">$5 per hour</p>
+              <p className="description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+              </p>
+            </div>
+          </article>
 
-      </div>
+          <article class="single-listing">
+            <img alt="guy holding wrench" src={Wrench}></img>
+            <div className="details">
+              <p className="listing-name">Wrench</p>
+              <p className="listing-price">$5 per hour</p>
+              <p className="description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+              </p>
+            </div>
+          </article>
+          
+          <article class="single-listing">
+            <img alt="guy holding wrench" src={Wrench}></img>
+            <div className="details">
+              <p className="listing-name">Wrench</p>
+              <p className="listing-price">$5 per hour</p>
+              <p className="description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+              </p>
+            </div>
+          </article>
+
+        </div>
+      </section>
     </div>
   );
 }
