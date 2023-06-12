df27c68feeeca8fdbad4f8d7d96c98116f3bcb59
ChrisPag <chrisp99@my.yorku.ca>
2023-06-12 01:33:02 -0400

    Added homepage responsiveness

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,33 +1,56 @@
 import './navbar.css';
 import Logo from '../../assets/logo-placeholder.png'
 import { NavLink } from 'react-router-dom'
+import { BiMenu } from "react-icons/bi";
+import React, { useState} from "react";
 function Navbar() {
-  return (
-    <div className="navbar">
-        
-        <ul className="pages">
-            <li>
+    const [showNav, setShowNav] = useState(false);
+    function handleMenu(){
+        setShowNav((showNav)=>!showNav);
+    }
+
+    return (
+        <div className="navbar">
+            <div className="large-navbar">
                 <img id="logo" alt="logo" src={Logo}></img>
-            </li>
-            <li>
-                <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-                <NavLink to="/search">Search</NavLink>
-            </li>
-        </ul>
-        <ul className="login">
-            <li>
-                <NavLink to="/login">Login</NavLink>
-            </li>
-            <li>
-                <NavLink to="/signup"><button>Sign Up</button></NavLink>
-            </li>
-        </ul>
-        
-        
-    </div>
-  );
+                <ul className="pages">
+                    
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/search">Search</NavLink>
+                    </li>
+                </ul>
+                <ul className="login">
+                    <li>
+                        <NavLink to="/login">Login</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/signup"><button>Sign Up</button></NavLink>
+                    </li>
+                </ul>
+                <BiMenu id="menu" onClick={handleMenu} />
+            </div>
+            {showNav &&
+            <div className="resize-navbar">
+                <ul>
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/search">Search</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/login">Login</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/signup"><button>Sign Up</button></NavLink>
+                    </li>
+                </ul>
+            </div>}
+        </div>
+    );
 }
 
 export default Navbar;
--- frontend/src/containers/home/Home.js
+++ frontend/src/containers/home/Home.js
@@ -1,4 +1,5 @@
 import './home.css';
+import FullHero from '../../assets/full-working.png';
 import Hero from '../../assets/working.png';
 import Hammer from '../../assets/hammer.jpg';
 import Wrench from '../../assets/wrench.jpg';
@@ -6,43 +7,52 @@ import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 
 function Home() {
-  /*test backend connection
+  /* //test backend connection
   //hold data here
-  const [message, setMessage] = useState("");
+  const [data, setData] = useState("");
 
   //access api endpoint
-  url="http://localhost:8080/";
+  const url="http://localhost:8080/getAllRentals";
 
   //when a component mounts (ie when it is inserted into the dom), call the api
   useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setMessage(data));
+    const fetchData = async() =>{
+      const response = await fetch(url);
+      const newData = await response.json();
+      setData(newData);
+  }
+      
+  fetchData();
 
   //empty array as second argument so this hook is only called once
-  }, []);*/
+  }, []); */
+
   return (
     <div className="home">
-      {/*<h1>{message}</h1>*/}
-      <section className="searchbar">
-        <p className="hero-text">Borrow and lend tools.</p>
-        <form>
-          <input type="text" placeholder="Search..." />
-          <select name="categories" id="categories">
-            <option value="" disabled selected>Categories</option>
-            <option value="hardware">Hardware</option>
-            <option value="cooking">Cooking</option>
-            <option value="gardening">Gardening</option>
-            <option value="cleaning">Cleaning</option>
-            <option value="arts">Arts & Crafts</option>
-          </select>
-          <button type="submit"><FaSearch /></button>
-        </form>
+      {/* <p>{data}</p> */}
+      <section>
+        <div className="heroResize">
+          <div className="searchbar">
+            <p className="hero-text">Borrow and lend tools.</p>
+            <form>
+              <input type="text" placeholder="Search..." />
+              <select name="categories" id="categories">
+                <option value="" disabled selected>Categories</option>
+                <option value="hardware">Hardware</option>
+                <option value="cooking">Cooking</option>
+                <option value="gardening">Gardening</option>
+                <option value="cleaning">Cleaning</option>
+                <option value="arts">Arts & Crafts</option>
+              </select>
+              <button type="submit"><FaSearch /></button>
+            </form>
+          </div>
+          <img alt="woodworking" src={FullHero}></img>
+        </div>
+        <div className="hero">
+          <img alt="woodworking" src={Hero}></img>
+        </div>
       </section>
-      <div className="hero">
-        <img alt="woodworking" src={Hero}></img>
-      </div>
-      
       <section className="listings">
         <h2>New Listings</h2>
         <div className="grid-container">
