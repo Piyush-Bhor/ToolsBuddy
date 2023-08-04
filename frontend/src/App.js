2f0ee15a20c4ae0fe0b5ea64e493dcf7111d636b
Yawar <yawarshaikh26@gmail.com>
2023-08-04 13:29:37 -0400

    Chat Assistant added -v1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,9 @@ import Account from "./containers/account/Account.js"
 import Navbar from "./components/Navbar.js"
 import Footer from "./components/Footer.js"
 import Rental from "./containers/rental/Rental.js"
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import ChatBot from './components/ChatBotBtn';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -16,19 +18,19 @@ function App() {
         </nav>
         <main>
           <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/search" element={<Search />}/>
-            <Route path="/rental/:id/:index" element={<Rental />}/>
-            <Route path="/account" element={<Account />}/>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/rental/:id/:index" element={<Rental />} />
+            <Route path="/account" element={<Account />} />
           </Routes>
         </main>
-        
+        <ChatBot />
         <footer>
           <Footer />
         </footer>
       </Router>
     </div>
-    
+
   );
 }
 
